635eef1d322b2d79470a683a19b87cc60b269a79
Amira-Assaf <70236658+Amira-Assaf@users.noreply.github.com>
2022-01-13 17:45:28 +0200

    faq

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -124,6 +124,63 @@
     </div>
 </section>
 
+<section class="text-right bg-light">
+    <div class="container">
+        <h2 class="clr-dark mb-4">الأسئلة الشائعة والتعليقات Newwwwwwwww</h2>
+        <p class="clr-gray mb-4">
+            <b>
+                قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني
+            </b>
+        </p>
+        <div id="accordion" class="accordion">
+            <div class="card mb-5">
+                <div class="box-shadow">
+                    <div class="card-header active" data-toggle="collapse" href="#collapseOne" aria-expanded="true">
+                        <a class="card-title accordion-title" dir="rtl">
+                            <span class="icon-paperclip"></span>
+                            كيفيه أداره الإعدادات
+                        </a>
+                    </div>
+                    <div id="collapseOne" class="card-body collapse show" data-parent="#accordion">
+                        <p class="clr-gray">
+                            هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال ا لمعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" (Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.
+                        </p>
+                    </div>
+                </div>
+                <div class="sprt"></div>
+                <div class="box-shadow">
+                    <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                        <a class="card-title  accordion-title" dir="rtl">
+                            <span class="icon-paperclip"></span>
+                            ماذا عن وسطاء الحدث والمنظمي ومكبرات الصوت ؟
+                        </a>
+                    </div>
+                    <div id="collapseTwo" class="card-body collapse" data-parent="#accordion">
+                        <p class="clr-gray">
+                            هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال ا لمعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" (Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.
+                        </p>
+                    </div>
+                </div>
+                <div class="sprt"></div>
+                <div class="box-shadow">
+                    <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                        <a class="card-title  accordion-title" dir="rtl">
+                            <span class="icon-paperclip"></span>
+                            ما هي بعض الامثله السيناريوهات ؟
+                        </a>
+                    </div>
+                    <div id="collapseThree" class="card-body collapse" data-parent="#accordion">
+                        <p class="clr-gray">
+                            هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال ا لمعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" (Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.
+                        </p>
+                    </div>
+                </div>
+                <div class="sprt"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <section>
     <div class="row justify-content-center pb-2 pt-5">
         <div class="col-md-8 text-center heading-section ftco-animate fadeInUp ftco-animated">
